88f298e9e8a8bce066d40fe14853120c114d9d44
JavadHPo <72570629+JavadHPo@users.noreply.github.com>
2020-12-14 19:40:15 -0700

    changes to code example

--- pemfc/pemfc_polarization.py
+++ pemfc/pemfc_polarization.py
@@ -2,15 +2,15 @@ import numpy as np
 from pemfc_model import pemfc_model
 
 def polarization(T=None):
-    print("temperature = ", T)
+#    print("temperature = ", T)
     i_array = np.linspace(1,20000,25)
     V_cell = np.zeros_like(i_array)
 
     for j, current in enumerate(i_array):
-        print('    i_ext = ',current)
+#        print('    i_ext = ',current)
         solution = pemfc_model(current,T)
         V_cell[j] = solution.y[-1,-1] - solution.y[0,-1]
-        print('        V_cell =', V_cell[j])
+#        print('        V_cell =', V_cell[j])
 
     return i_array, V_cell
 
